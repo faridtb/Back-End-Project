3c667bdcd3f512052a05aa1bcbca2e969be3de59
faridtb <faridtb@code.edu.az>
2022-07-27 15:11:49 +0400

    refactor: basket viewcomponent has been changed

--- Allup/ViewComponents/BasketViewComponent.cs
+++ Allup/ViewComponents/BasketViewComponent.cs
@@ -1,6 +1,7 @@
 ﻿using Allup.DAL;
 using Allup.Models;
 using Allup.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -14,43 +15,55 @@ namespace Allup.ViewComponents
     public class BasketViewComponent : ViewComponent
     {
         private readonly AppDbContext _context;
-
-        public BasketViewComponent(AppDbContext context)
+        private readonly UserManager<User> _userManager;
+        public BasketViewComponent(AppDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            string basket = Request.Cookies["basket"];
+
+            var userId = _userManager.GetUserId(Request.HttpContext.User);
+
             double totalPrice = 0;
             int totalCount = 0;
 
-            List<BasketVM> products;
+            Basket basket;
 
-            if (basket != null)
+            if (userId == null)
             {
-                products = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
-
-                foreach (BasketVM item in products)
-                {
-                    Product product = _context.Products.Include(p => p.ProductImages).FirstOrDefault(p => p.Id == item.Id);
-                    item.Price = product.Price;
-                    item.Name = product.Name;
-                    item.ImgUrl = product.ProductImages.Find(p => p.IsMain == true).ImageUrl;
-                    item.SubTotalPrice += product.Price * item.Count;
-                    totalPrice += item.SubTotalPrice;
-                    totalCount += item.Count;
-                }
+                basket = new Basket();
             }
             else
             {
-                products = new List<BasketVM>();
+                basket = _context.Baskets
+                .Include(b => b.BasketItems)
+                .ThenInclude(b => b.Product)
+                .ThenInclude(b => b.ProductImages)
+                .FirstOrDefault(b => b.UserId == userId);
+
+                if (basket == null)
+                {
+                    basket = new Basket() { UserId = userId };
+                    basket.BasketItems = new List<BasketItem>();
+                    _context.Add(basket);
+                    _context.SaveChanges();
+                }
+
+                foreach (var item in basket.BasketItems)
+                {
+                    totalPrice += item.ProductCount * item.Product.Price;
+                    totalCount += item.ProductCount;
+                }
             }
+
             ViewBag.TotalPrice = totalPrice;
             ViewBag.TotalCount = totalCount;
-            return View(await Task.FromResult(products));
+
+            return View(await Task.FromResult(basket));
         }
     }
 }
